d21a1d3befcd3db7ec2e0f6f1a59acd6929cbfaf
CaitlinKennedy444 <mhy5@cdc.gov>
2017-01-27 14:52:52 -0500

    Run Analysis

--- run_analysis.R
+++ run_analysis.R
@@ -23,25 +23,19 @@ getwd()
 
 
 #Read in all data from all the files and subfolders 
-test_subject_id <- read.table('./test/subject_test.txt' ,header=FALSE);
+test_subject_id <- read.table('./test/subject_test.txt' ,header=FALSE,col.names="subject_id");
 testdata <- read.table('./test/X_test.txt' ,header=FALSE);
-test_activity_id <- read.table('./test/y_test.txt' ,header=FALSE);
-train_subject_id <- read.table('./train/subject_train.txt' ,header=FALSE);
+colnames(testdata) <- feature_names;
+test_activity_id <- read.table('./test/y_test.txt' ,header=FALSE,col.names="activity_id");
+train_subject_id <- read.table('./train/subject_train.txt' ,header=FALSE,col.names="subject_id");
 traindata <- read.table('./train/X_train.txt' ,header=FALSE);
-train_activity_id <- read.table('./train/y_train.txt',header=FALSE);
+colnames(traindata) <- feature_names;
+train_activity_id <- read.table('./train/y_train.txt',header=FALSE,col.names="activity_id");
 activity_labels <- read.table('./activity_labels.txt',header=FALSE,col.names=c("activity_id", "activity_name"));
 features <- read.table('./features.txt', header=FALSE);
 features <- read.table("features.txt", strip.white=TRUE, stringsAsFactors = FALSE) #Answers_what are the features in the data? 
 feature_names <-features[,2]
 #All data has been read in from all files 
-
-#Make column titles more appropriate to match what they really are
-colnames(train_subject_id) <- "subject_id";
-colnames(traindata) <- feature_names;
-colnames(train_activity_id) <- "activity_id";
-colnames(test_subject_id) <- "subject_id";
-colnames(testdata) <- feature_names;
-colnames(test_activity_id) <- "activity_id";
 
 #Bind together the columns representing training data and separately bind together the columns representing the test data;
 traindata <- cbind(train_subject_id,train_activity_id,traindata);
@@ -58,61 +52,62 @@ head(mergeData)
 names(mergeData)
 columnNames <- colnames(mergeData)
 head(columnNames)
-
-mean_col_idx <- grep("mean",names(mergeData),ignore.case=TRUE) 
-mean_col_names <- names(mergeData)[mean_col_idx] 
-std_col_idx <- grep("std",names(mergeData),ignore.case=TRUE) 
-std_col_names <- names(mergeData)[std_col_idx] 
-meanstddata <-mergeData[,c("subject_id","activity_id",mean_col_names,std_col_names)] 
-descrnames <- merge(activity_labels,meanstddata,by.x="activity_id",by.y="activity_id",all=TRUE)
-
 #End of step one, training and testing datasets have been merged and a column names identifier has been created for easier processing
 
 #Step 2- Data has been merged but there are a lot of extra variables that we do not need to consider in this analysis. We want
 #to get rid of those extra variables and only keep variables pertaining to the suject ID, mean and standard deviations. Thus
 #to to this, we need to tell R which variables are useful (TRUE) and which variables are unuseful (!FALSE)
 #grep is like keep/drop statement in sas, the "..."function will tell R to select variables with anything similar to "mean" or "std" in the title. The ! will tell it to not select those variables
-variableselection <- (grepl("activity..",columnNames) | grepl("subject..",columnNames) | grepl("-mean..",columnNames) & !grepl("-meanFreq..",columnNames) & !grepl("mean..-",columnNames) | grepl("-std..",columnNames) & !grepl("-std()...-",columnNames));
+#variableselection <- (grepl("activity..",columnNames) | grepl("subject..",columnNames) | grepl("-mean..",columnNames) & !grepl("-meanFreq..",columnNames) & !grepl("mean..-",columnNames) | grepl("-std..",columnNames) & !grepl("-std()...-",columnNames));
 #Tell R to only keep the variables pulled from GREP that are TRUE
-selecteddata <-mergeData[variableselection==TRUE];
-selecteddata
-names(selecteddata)
+#selecteddata <-mergeData[variableselection==TRUE];
+#selecteddata
+#names(selecteddata)
+#Pull/GREP all columns relevant to means (mean is in the name of the column) 
+meanincolname <- grep("mean",names(mergeData),ignore.case=TRUE) 
+#Put that pulled data of means information in a dataset called meanincolname 
+meanincolname <- names(mergeData)[meanincolname] 
+#Pull/GREP all columns relevant to std (using grep)
+stdincolname <- grep("std",names(mergeData),ignore.case=TRUE) 
+stdincolname <- names(mergeData)[stdincolname] 
+#Summarize specific means and standard deviations for subject id and activity id based on the merged dataset (pulling variables of use from train and test datasets only)
+summarydata <-mergeData[,c("subject_id","activity_id",meanincolname,stdincolname)] 
+#merge the data with the activity labels with the summary data (which up to that point only contains the training and test datasets)
+completedata <- merge(activity_labels,summarydata,by.x="activity_id",by.y="activity_id",all=TRUE)
 
 #Step 3- At this point only the data files relating to test and train have been merged. We called in the activity types dataset,
 #but have not done anything with it yet. Here, lets merge the activity dataset with the other dataset we have been working with up 
 #to this point (titled selecteddata based on the processing done in step 2)
 #Also note, the activity labels in the activities dataset are: walking2, walking_upstairs3, walking_downstairs4, sitting5, standing6 and laying
-finalData <- merge(selecteddata,activity_labels,all=TRUE)
-names(finalData) #use this to look at all column names
-unique(finalData$activity_name) #use this to look at all subheadings of a variable (simple proc freq but just with variable titles)
+unique(completedata$activity_name) #use this to look at all subheadings of a variable (simple proc freq but just with variable titles)
 labels <- read.table("activity_labels.txt", stringsAsFactors = FALSE) #Same use as Uniue()
 labels #Labels is same as Unique
 #Remove the underscores from the activityTypes and create descriptive activity names to name the activities in the dataset
-finalData$activity_name<-as.character(sub("_","", finalData$activity_name))
-unique(finalData$activity_name)
+completedata$activity_name<-as.character(sub("_","", completedata$activity_name))
+unique(completedata$activity_name)
 #update the column names after the merge
-columnNames <- colnames(finalData)
+columnNames <- colnames(completedata)
 #Step 3 is complete, descriptive activity names have been updated in the dataset
 
 #Step 4-Clean up column names to something more readable
-names(finalData) #here is what the column names look like now
+names(completedata) #here is what the column names look like now
 #We want to  rename the columns so that anything with "-mean" in it at all will come up as "Mean" and anything 
 #with "-std" in it at all will come up as simply "standard deviation", etc. Basically we need to make all the column titles more readable.
 
 #Rename column titles using the names and gsub commands
-names(finalData)<-gsub("Acc", "Accelerometer", names(finalData))
-names(finalData)<-gsub("Gyro", "Gyroscope", names(finalData))
-names(finalData)<-gsub("BodyBody", "Body", names(finalData))
-names(finalData)<-gsub("Mag", "Magnitude", names(finalData))
-names(finalData)<-gsub("^t", "Time", names(finalData))
-names(finalData)<-gsub("^f", "Frequency", names(finalData))
-names(finalData)<-gsub("tBody", "TimeBody", names(finalData))
-names(finalData)<-gsub("-mean()", "Mean", names(finalData), ignore.case = TRUE)
-names(finalData)<-gsub("-std()", "STD", names(finalData), ignore.case = TRUE)
-names(finalData)<-gsub("-freq()", "Frequency", names(finalData), ignore.case = TRUE)
-names(finalData)<-gsub("angle", "Angle", names(finalData))
-names(finalData)<-gsub("gravity", "Gravity", names(finalData))
-names(finalData)
+names(completedata)<-gsub("Acc", "Accelerometer", names(completedata))
+names(completedata)<-gsub("Gyro", "Gyroscope", names(completedata))
+names(completedata)<-gsub("BodyBody", "Body", names(completedata))
+names(completedata)<-gsub("Mag", "Magnitude", names(completedata))
+names(completedata)<-gsub("^t", "Time", names(completedata))
+names(completedata)<-gsub("^f", "Frequency", names(completedata))
+names(completedata)<-gsub("tBody", "TimeBody", names(completedata))
+names(completedata)<-gsub("-mean()", "Mean", names(completedata), ignore.case = TRUE)
+names(completedata)<-gsub("-std()", "STD", names(completedata), ignore.case = TRUE)
+names(completedata)<-gsub("-freq()", "Frequency", names(completedata), ignore.case = TRUE)
+names(completedata)<-gsub("angle", "Angle", names(completedata))
+names(completedata)<-gsub("gravity", "Gravity", names(completedata))
+names(completedata)
 #Here is a summary of the changes that were made after we changed the column names
 # Acc was replaced by Accelerometer
 # Gyro was replaced by Gyroscope
@@ -126,7 +121,7 @@ names(finalData)
 # All iterations of "-freq()" were replaced by "Frequency
 # angle was replaced by Angle
 # gravity was replaced by Gravity
-columnNames <- colnames(finalData)
+columnNames <- colnames(completedata)
 #Step 4 is complete
 
 #Step 5- From the dataset in step 4, create a second, independent tidy data set with the average of each variable for each 
@@ -137,7 +132,7 @@ columnNames <- colnames(finalData)
 #first install reshape2 package
 install.packages("reshape2")
 library(reshape2)
-num5melt <- melt(descrnames,id=c("activity_id","activity_name","subject_id"))
+num5melt <- melt(completedata,id=c("activity_id","activity_name","subject_id"))
 tidydata <- dcast(num5melt, activity_id + activity_name + subject_id ~ variable,mean)
 tidydata
 #Step 5 is done
